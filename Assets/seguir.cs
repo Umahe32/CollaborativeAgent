110dc4fddf3a6e28f6fcd4fb1d5cf096deb7ac48
Umahe32 <michael.palacios@correounivalle.edu.co>
2021-03-19 12:33:03 -0500

    Add files via upload
    se añanden las animaciones especiales

---
+++ Assets/seguir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seguir : MonoBehaviour
+{
+
+    public GameObject companion;
+    public Transform izquierda;
+    public Transform derecha;
+    public Transform centro;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //companion.transform.parent = null;
+        izquierda.SetParent(companion.transform);
+        derecha.SetParent(companion.transform);
+        centro.SetParent(companion.transform);
+    }
+
+}
